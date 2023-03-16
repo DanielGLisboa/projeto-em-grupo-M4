e52dca20c063b6c0f9bb017dc616057290b7148d
Daniel Lisboa <daniel.lisbooa@gmail.com>
2023-03-16 12:11:23 -0300

    Iniciando back-end

---
+++ back-end/ValdisneiStreaming/src/test/java/com/api/ValdisneiStreaming/ValdisneiStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ValdisneiStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValdisneiStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
